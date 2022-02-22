c44a4a2bd68f551328f5e4a89bf75ae8f5e90f52
dounya-m <mellouki.dounya@gmail.com>
2022-02-22 10:34:39 +0100

    update

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-
+    include_once './views/includes/navBare.php';
 ?>
 <?php
 
@@ -9,3 +9,33 @@
     print_r($voyages);
 
 ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Départ</th>
+                        <th scope="col">Arrivée</th>
+                        <th scope="col">de</th>
+                        <th scope="col">à</th>
+                        <th scope="col">N° du Train</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                        <th scope="row"></th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
