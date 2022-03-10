aa1a977a95cab283bfc92d6ae096b1f4cfd53c7a
dounya-m <mellouki.dounya@gmail.com>
2022-03-10 14:09:05 +0100

    update

--- views/landing.php
+++ views/landing.php
@@ -1,7 +1,6 @@
-<?php
-        include_once './public/style/main.scss';
+<?php 
+    include_once './views/includes/navBare.php';
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,9 +8,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/style/main.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>HELLO</h1>
+<section class="head">
+    <h1>Wher do you want to go?</h1>
+    <div class="head_button">
+    <button type="button" class="btn btn-primary btn-lg">Sign Up</button>
+    <button type="button" class="btn btn-outline-light btn-lg">Sign In</button>
+    </div>
+    <div class="head-description d-flex flex-column">
+    <div class= "d-flex">
+            <h3>120m+</h3>
+            <h3>36</h3>
+            <h3>80</h3>
+            </div>
+        <div class= "d-flex">
+            <p>Passengers</p>
+            <p>Station</p>
+            <p>Trains</p>
+        </div>
+    </table>
+    </div>
+</section>
+
+
+    <?php 
+    include_once './views/includes/footer.php';
+?>
 </body>
 </html>
