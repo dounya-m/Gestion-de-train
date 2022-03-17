4ae2b60982c3a37139d9aaf8fb842596f17890c6
dounya-m <mellouki.dounya@gmail.com>
2022-03-17 16:35:29 +0100

    update

--- views/dashboard.php
+++ views/dashboard.php
@@ -9,43 +9,43 @@
     $voyages = $data->getAllVoyage();
 
 ?>
-    <section class= d-flex >
-<div class="container">
+    <section class= "d-flex" >
+<div class="container col-12">
     <div class="row">
-        <div class="col mt-5  mx-auto">
+        <div class=" mt-5  mx-auto">
         <div class="card">
                 <div class="card-body">
             <a href="<?php echo BASE_URL;?>add" class="">
                 <i class="fas fa-plus"></i>
             </a>
-            <table class="table mt-5 table-hover">
+            <table class="table  mt-5 table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Date</th>
-                        <th scope="col">Départ</th>
-                        <th scope="col">Arrivée</th>
-                        <th scope="col">de</th>
-                        <th scope="col">à</th>
-                        <th scope="col">N° du Train</th>
-                        <th scope="col">Nom Train</th>
-                        <th scope="col">1ere classe</th>
-                        <th scope="col">2eme classe</th>
-                        <th scope="col">Prix</th>
+                        <th class="text-center" scope="col">Départ</th>
+                        <th class="text-center" scope="col">Arrivée</th>
+                        <th class="text-center" scope="col">de</th>
+                        <th class="text-center" scope="col">à</th>
+                        <th class="text-center" scope="col">N° du Train</th>
+                        <th class="text-center" scope="col">Nom Train</th>
+                        <th class="text-center" scope="col">1ere classe</th>
+                        <th class="text-center" scope="col">2eme classe</th>
+                        <th class="text-center" scope="col">Prix</th>
                     </tr>
                 </thead>
                 <tbody>
                 <?php foreach($voyages as $voyage):?>
                 <tr>
                     <th scope="row"><?php echo $voyage['date']; ?></th>
-                    <td><?php echo $voyage['heur_depart']; ?></td>
-                    <td><?php echo $voyage['heur_arriver']; ?></td>
-                    <td><?php echo $voyage['gare_dep']; ?></td>
-                    <td><?php echo $voyage['gare_arr']; ?></td>
-                    <td><?php echo $voyage['train']; ?></td>
-                    <td><?php echo $voyage['nom']; ?></td>
-                    <td><?php echo $voyage['first_class']; ?></td>
-                    <td><?php echo $voyage['second_class']; ?></td>
-                    <td><?php echo $voyage['prix']; ?>dh</td>
+                    <td class="text-center"><?php echo $voyage['heur_depart']; ?></td>
+                    <td class="text-center"><?php echo $voyage['heur_arriver']; ?></td>
+                    <td class="text-center"><?php echo $voyage['gare_dep']; ?></td>
+                    <td class="text-center"><?php echo $voyage['gare_arr']; ?></td>
+                    <td class="text-center"><?php echo $voyage['train']; ?></td>
+                    <td class="text-center"><?php echo $voyage['nom']; ?></td>
+                    <td class="text-center"><?php echo $voyage['first_class']; ?></td>
+                    <td class="text-center"><?php echo $voyage['second_class']; ?></td>
+                    <td class="text-center"><?php echo $voyage['prix']; ?>dh</td>
 
                     
                 <td class="d-flex">
--- views/reservation.php
+++ views/reservation.php
@@ -3,5 +3,79 @@
 ?>
 
 <body>
-    <section></section>
+
+
+<section class="info d-flex justify-content-center mt-5 mx-auto col-6">
+    <h3>Csablanca</h3>
+    <h3>Oujda</h3>
+    <div class="prix">
+        <p class="mt-3">120Dh</p>
+    </div>
+</section>
+
+    <section class="voyageurInfo mx-auto col-10 col-lg-6 col-md-9 mt-5">
+    <div>
+        <h1>Qui voyage ?</h1>
+        <h4>J'enregistre les informations voyageurs</h4>
+</div>
+        <form method="post" class="information_form d-flex flex-column mt-5 align-items-center" id="signUp">
+
+<div class="d-flex justify-content-center mt-5">
+<div class="form-group mr-5">
+        <label for="inputnom">Mon nom</label>
+        <input type="text" class="form-control" id="inputEmail" name="nom" placeholder="nom">
+    </div>
+    <div class="form-group ml-5">
+        <label for="inputprenom">Mon prénom</label>
+        <input type="text" class="form-control" id="inputEmail" name="prenom" placeholder="prénom">
+    </div>
+    </div>
+
+    <div class="d-flex justify-content-center mt-5">
+    <div class="form-group mr-5">
+        <label for="inputdate">Numéro de Téléphone</label>
+        <input type="text" class="form-control" id="inputEmail" name="date" placeholder="Téléphone">
+    </div>
+    <div class="form-group ml-5">
+        <label for="inputdate">Mon email</label>
+        <input type="email" class="form-control" id="inputEmail" name="date" placeholder="Téléphone">
+    </div>
+    </div>
+    <!-- <input type="submit" name="submit" class="btn btn-primary mt-2 "></button> -->
+    </section>
+
+    <section class="voyageurInfo mx-auto col-10 col-lg-4 col-md-9 mt-5 mb-5">
+    <div>
+        <h1>Demande de paiement</h1>
+</div>
+
+    <div method="post" class="information_form d-flex flex-column mt-5 align-items-center" id="signUp">
+
+<div class="d-flex flex-column justify-content-center mt-2">
+<div class="form-group mr-2">
+        <label for="inputnom">Nom du porteur de la carte</label>
+        <input type="text" class="form-control" id="inputEmail" name="nom" placeholder="écrire...">
+    </div>
+    <div class="form-group ml-2">
+        <label for="inputprenom">Numéro de carte de paiement</label>
+        <input type="text" class="form-control" id="inputEmail" name="prenom" placeholder="écrire...">
+    </div>
+    <div class="form-group ml-2">
+        <label for="inputprenom">Date d'expiration</label>
+        <input type="date" class="form-control" id="inputEmail" name="prenom" placeholder="prénom">
+    </div>
+    <div class="form-group ml-2">
+        <label for="inputprenom">Code de verification</label>
+        <input  type="text" class="form-control" id="inputEmail" name="prenom" placeholder="code..">
+    </div>
+    </div>
+
+    <input type="submit" name="submit" class="btn btn-primary mt-2 "></button>
+</div>
+
+</form>
+    </section>
+    <?php
+    include_once './views/includes/footer.php';
+    ?>
 </body>
